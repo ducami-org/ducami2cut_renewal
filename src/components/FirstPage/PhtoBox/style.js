9c7f53ae47715ff8dba1c6f6f4e4e2b628ddd735
이윤하 <leeyoonha2015@gmail.com>
2025-06-02 22:47:26 +0900

    feat :: create photo animation

--- src/components/FirstPage/PhtoBox/style.js
+++ src/components/FirstPage/PhtoBox/style.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 const fadeInTop = keyframes`
     from{
@@ -8,18 +8,44 @@ const fadeInTop = keyframes`
         opacity: 1;
         transform: translateY(0);
     }
-`
+`;
+const fadeInBottom = keyframes`
+    from{
+        opacity: 0;
+        transform: translateY(50px);
+    }to{
+        opacity: 1;
+        transform: translateY(0px);
+    }
+`;
 
 export const Container = styled.div`
-    display:flex;
-    gap:40px;
-`
+  display: flex;
+  gap: 40px;
+`;
 export const PhotoWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap:40px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
 export const Photo = styled.img`
-    width: 248px;
-    animation : ${fadeInTop} 1.2s ease;
-`
+  width: 248px;
+  opacity: 0;
+  animation: ${({ $aniNum }) =>
+    css`
+      ${$aniNum === 1
+        ? fadeInBottom
+        : $aniNum === 2
+        ? fadeInTop
+        : $aniNum === 3
+        ? fadeInTop
+        : fadeInBottom} 1.5s ease ${$aniNum === 1
+        ? "0.2s"
+        : $aniNum === 2
+        ? "0s"
+        : $aniNum === 3
+        ? "0.6s"
+        : "0.8s"};
+    `};
+    animation-fill-mode:forwards;
+`;
--- src/components/common/CustomBtn/style.js
+++ src/components/common/CustomBtn/style.js
@@ -14,4 +14,5 @@ export const ButtonBox = styled.button`
     &:hover{
         transform: scale(1.1);
     }
+
 `
