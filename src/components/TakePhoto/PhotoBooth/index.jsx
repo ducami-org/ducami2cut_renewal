a07919b9af5e6a082aed95585c53db536f43bc7b
이윤하 <leeyoonha2015@gmail.com>
2025-06-08 02:14:06 +0900

    feat :: create photo taking function

--- src/components/TakePhoto/PhotoBooth/index.jsx
+++ src/components/TakePhoto/PhotoBooth/index.jsx
@@ -1,13 +1,26 @@
 import React, { useRef, useState } from "react";
 import * as S from "./style";
 import TakeBtn from "../TakeBtn";
+import { usePhotoStore } from "../../../store/PhotoInfo";
 
 const PhotoBooth = () => {
-    const webcamRef = useRef(null);
+  const webcamRef = useRef(null);
+  const setPhoto = usePhotoStore((state) => state.actions.setPhoto);
+  const photo = usePhotoStore((state)=> state.photo);
+  const [takeCnt, setTakeCnt] = useState(0);
+
+  const captureHandle = () => {
+    if (takeCnt <= 1) {
+      const img = webcamRef.current.getScreenshot();
+      setPhoto(takeCnt, img);
+      setTakeCnt(takeCnt + 1);
+      console.log(photo);
+    }
+  };
   return (
     <S.BoothWrapper>
-      <S.Camera mirrored={true} />
-      <TakeBtn />
+      <S.Camera mirrored={true} ref={webcamRef} />
+      <TakeBtn onClick={captureHandle} />
     </S.BoothWrapper>
   );
 };
--- src/store/PhotoInfo.js
+++ src/store/PhotoInfo.js
@@ -1,14 +1,19 @@
 import { create } from "zustand";
 
-export const usePhotoStore = create((set)=>({
-    frameColor:'',
-    frameText:'',
-    bgPhoto:'',
-    photo:[],
-    actions:{
-        setFrameColor: (color)=> set({frameColor:color}),
-        setFrameText: (text) => set({frameText:text}),
-        setBgPhoto: (photo) => set({bgPhoto:photo}),
-        setPhoto: (photo1,photo2) => set({photo:[photo1,photo2]}),
-    }
-}))
+export const usePhotoStore = create((set) => ({
+  frameColor: "",
+  frameText: "",
+  bgPhoto: "",
+  photo: [],
+  actions: {
+    setFrameColor: (color) => set({ frameColor: color }),
+    setFrameText: (text) => set({ frameText: text }),
+    setBgPhoto: (photo) => set({ bgPhoto: photo }),
+    setPhoto: (index, newPhoto) =>
+      set((state) => {
+        const updated = [...state.photo];
+        updated[index] = newPhoto;
+        return { photo: updated };
+      }),
+  },
+}));
